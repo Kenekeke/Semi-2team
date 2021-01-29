5823622917b98ea388060b8cef78f20e3f895463
mikyo <mikyo@DESKTOP-BQJNF2K>
2021-01-29 15:19:34 +0900

    BoardDao 수정

--- semiHouse/src/houseSemi/beans/BoardDao.java
+++ semiHouse/src/houseSemi/beans/BoardDao.java
@@ -356,21 +356,5 @@ public class BoardDao {
 		ps.execute();
 		con.close();
 	}
-	public BrokerDto find(String city) throws Exception{
-		Connection con = JdbcUtil.getConnection(USERNAME, PASSWORD);
-		String sql = "select * from broker where instr(broker_address, ?)>0";
-		PreparedStatement ps = con.prepareStatement(sql);
-		ps.setString(1, city);
-		ResultSet rs = ps.executeQuery();
-		BrokerDto dto;
-		if(rs.next()) {
-			dto = new BrokerDto();
-			dto.setBroker_no(rs.getInt("broker_no"));
-			dto.setBroker_address(rs.getString("broker_address"));
-		}else {
-			dto = null;
-		}
-		con.close();
-		return dto;
-	}
+
 }
